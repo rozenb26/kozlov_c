b9f1a4b834053b254b989cea6a18ca303f4328e2
blkn <rozenb26@gmail.com>
2026-04-01 15:16:51 +0300

    GOOD

--- practice01/task20.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int q, w, e;
-    scanf("%d %d %d", &q, &w, &e);
-    if (q+w==e) printf("da");
-    else printf("ne");
-}
--- practice05/task01.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    char str[] = "hEllo wOrld";
-    int len = sizeof(str) / sizeof(str[0]);
-    for (int i = len - 1; i >= 0; i--) {
-    printf("%c", str[i]);
-    }
-}
